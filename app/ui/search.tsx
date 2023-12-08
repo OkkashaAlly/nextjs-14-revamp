1e5f223aa662de353cbb8fdb9013ce8816d0b0ca
OkkashaAlly <steamnature006@gmail.com>
2023-12-08 15:15:27 +0300

    Chapter 11 - Adding Search and Pagination: Best practice: Debouncing - fixed to user form submit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,20 +2,28 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 
 // ========================================================================
 // SEARCH COMPONENT =======================================================
 // ========================================================================
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+
+  const [inputValue, setInputValue] = useState(
+    searchParams.get('query')?.toString() || '',
+  );
+
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+    
     const param = new URLSearchParams(searchParams);
 
-    if (term) {
-      param.set('query', term);
+    if (inputValue) {
+      param.set('query', inputValue);
     } else {
       param.delete('query');
     }
@@ -25,7 +33,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   // RETURN =================================================================
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <form
+      onSubmit={handleSearch}
+      className="relative flex flex-1 flex-shrink-0"
+    >
       <label htmlFor="search" className="sr-only">
         Search
       </label>
@@ -33,11 +44,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value);
+          setInputValue(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        value={inputValue}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
+      <MagnifyingGlassIcon
+        onClick={handleSearch}
+        className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+      />
+    </form>
   );
 }
