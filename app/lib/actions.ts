036d21783844c43fd3e9fd99d386d7ef11e0b691
OkkashaAlly <steamnature006@gmail.com>
2023-12-11 15:05:28 +0300

    Chapter 12 - Updating an invoice: 4. Pass the id to the Server Action and Update invoices id DB

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,5 @@
 'use server';
+
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -35,6 +36,30 @@ export async function createInvoice(formData: FormData) {
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// UPDATE INVOICES ==========================================================
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
