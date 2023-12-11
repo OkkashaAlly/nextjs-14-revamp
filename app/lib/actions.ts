377552faafdf59421266a7808a3edf5baf09e325
OkkashaAlly <steamnature006@gmail.com>
2023-12-11 14:46:32 +0300

    Chapter 12 - Mutating Data: 6. Revalidate and redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 const FormSchema = z.object({
@@ -32,4 +34,7 @@ export async function createInvoice(formData: FormData) {
   await sql`INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
