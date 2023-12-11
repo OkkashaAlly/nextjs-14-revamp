fb4092961db9f0403d277c1d8aa760ec349eb520
OkkashaAlly <steamnature006@gmail.com>
2023-12-11 14:34:24 +0300

    Chapter 12 - Mutating Data: 3. Extract the data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,14 @@
 'use server';
 
 // CREATE INVOICES =========================================
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+  // if you have many fields 
+  // const rawFormData = Object.fromEntries(formData.entries())
+  
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  console.log(rawFormData);
+}
