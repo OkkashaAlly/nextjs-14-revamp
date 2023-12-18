fa3785e1bf5b8c2e5e115ca32412169c3153f98e
OkkashaAlly <steamnature006@gmail.com>
2023-12-18 12:21:43 +0300

    Chapter 16 - Adding Metadata:Page title and descriptions

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,12 @@ import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 // ===============================================================================
 // INVOICE PAGE ==================================================================
